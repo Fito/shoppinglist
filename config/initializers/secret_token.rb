78b02200b41b753684287356004b3d8703bd9b7e
Fito <fito@paragil.com>
2013-02-19 18:27:15 -0800

    Initial commit, Rails and Gemfile setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingList::Application.config.secret_token = '97f9604db83e556d249367f987d6f19c911b72e1369edf453bbc796be6863d46b6dec8051404d30c0a851ff3238fd199240ea3e94f2db3abce2cc4074a6cf901'
