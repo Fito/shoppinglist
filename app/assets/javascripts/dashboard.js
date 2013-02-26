6ed32503dda9dd3613760fe5b5fa0af0332a5061
Fito <fito@paragil.com>
2013-02-25 23:27:14 -0800

    asynchronous delete and done added

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,18 +1,55 @@
 $(function init(){
-  $('div.item h5').click( function(){
+  $('div.item').click( function(){
     var self = $(this);
-    if (self.parent().height() < 78){
-      self.parent().height(78);
+    if (self.height() < 78){
+      self.height(78);
     } else {
-      self.parent().height(40);
+      self.height(40);
     }
-  });
-  
-  $('div.item button.more-info').click( function(){
-    var self = $(this);
-    var url = 'items/' + self.attr('id') + '.json'
-    $.get(url, function(data){
-      alert(data["description"]);
+    
+    $('div.item button.more-info').click( function(){
+      var self = $(this);
+      var url = 'items/' + self.parent().attr('id') + '.json';
+      $.get(url, function(data){
+        alert(data["description"]);
+      });
     });
-  });
+    
+    $('div.item button.delete').click( function(){
+      var self = $(this);
+      var url = 'items/' + self.parent().attr('id') + '.json';
+      var top = self.parent().offset().top;
+      $('div.warning-message').show();
+      $('div.warning-message').css('top', top);
+      $('button.warning-yes').click( function(){
+        $('div.warning-message').hide();
+        self.parent().remove();
+        $.ajax({ url: url, type: 'DELETE'});
+      });
+      $('button.warning-cancel').click( function(){
+        $('div.warning-message').hide();
+      })
+    });
+
+    $('div.item button.done').click( function(){
+      var self = $(this);
+      var text = $('h5', $(self.parent())).text();
+      $('h5', $(self.parent())).html('<s>' + text + '</s>');
+      self.parent().css('background-color', '#FF8C20');
+      self.parent().css('border-color', '#FF8C20');
+      self.html('Undo');
+      self.removeClass('done').addClass('undo');
+    });  
+    
+    
+    $('div.item button.undo').click( function(){
+      var self = $(this);
+      var text = $('h5', $(self.parent())).text();
+      $('h5', $(self.parent())).text(text);
+      self.parent().css('background-color', '#F6FA9C');
+      self.parent().css('border-color', '#F6FA9C');
+      self.html('Done');
+      self.removeClass('undo').addClass('done');
+    });
+  });  
 });
